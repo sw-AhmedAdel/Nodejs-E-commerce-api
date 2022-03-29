ba407bc399879e0bed0a28fb79c8a50b2a6c9f24
Ahmed <sw.ahmedadel@gmail.com>
2022-03-29 15:33:23 +0200

    add product shcema

---
+++ server/src/models/products.models.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const productScheam = new mongoose.Schema({
+  name : {
+    type : String,
+    required: [true, 'Product name must be provided'],
+  },
+  price: {
+    type : Number,
+    required:[true, 'Product price must be provided '],
+  },
+  ratings: {
+    type :Number,
+    default:0,
+    min: ['Ratin1 ,g must be above 1'],
+    max: [5,, 'Rating must be bellow or equal 5.0'],
+  },
+  featured : {
+    type: Boolean,
+    default: false,
+  },
+  company :{
+    type : String,
+    required:[true , 'Product must have a company name'],
+    enum: {
+      values: ['ikea','liddy','caressa','marcos'],
+      message: '({VAKUE}) is not supported' ,
+    }
+  }
+  ,
+  createdAt : {
+    type : Date ,
+    default: Date.now(),
+  }
+});
+
+const products = mongoose.model('Product' , productScheam);
+module.exports = products;
